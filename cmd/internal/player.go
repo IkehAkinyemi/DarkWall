651d672719471b70437c746dd8050e60f2d8a26c
IkehAkinyemi <mrikehchukwuka@gmail.com>
2022-11-04 00:08:54 +0100

    feat: Added controls to increase/decrease volume, & stop music

--- cmd/internal/player.go
+++ cmd/internal/player.go
@@ -7,8 +7,11 @@ import (
 	"time"
 
 	"github.com/faiface/beep"
+	"github.com/faiface/beep/effects"
 	"github.com/faiface/beep/mp3"
 	"github.com/faiface/beep/speaker"
+
+	termbx "github.com/nsf/termbox-go"
 )
 
 // player retrieve music file, decodes and stream the data
@@ -28,15 +31,40 @@ func Player(musicPath string) {
 	//Initialize speakers
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
+	// Setup basic controls for pause/resume music and increase/decrease vol.
 	controls := &beep.Ctrl{Streamer: beep.Loop(-1, streamer), Paused: false}
+	vol := effects.Volume{
+		Streamer: controls,
+		Base: 2,
+		Volume: 0,
+		Silent: false,
+	}
 	speaker.Play(controls)
 
-	log.Print("Press [ENTER] to pause/resume")
+	termboxErr := termbx.Init()
+	if termboxErr != nil {
+		log.Fatal(termboxErr)
+	}
+	defer termbx.Close()
+
+	fmt.Println("Use [ENTER] to pause/resume: [ENTER]")
+	fmt.Println("Press up/down keys to alter volume: [↓ ↑]")
+	fmt.Println("Press escape key to exit DarkWalls: [Esc]")
 
 	for {
-		fmt.Scanln()
-		speaker.Lock()
-		controls.Paused = !controls.Paused
-		speaker.Unlock()
+		keyEvent := termbx.PollEvent()
+
+		switch {
+		case keyEvent.Key == termbx.KeyEnter:
+			controls.Paused = !controls.Paused
+		case keyEvent.Key == termbx.KeyArrowUp:
+			fmt.Print("pressed up")
+			vol.Volume += 0.5
+		case keyEvent.Key == termbx.KeyArrowDown:
+			fmt.Print("pressed down")
+			vol.Volume -= 0.5
+		case keyEvent.Key == termbx.KeyEsc:
+			os.Exit(0)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.19
 require (
 	github.com/faiface/beep v1.1.0
 	github.com/manifoldco/promptui v0.9.0
+	github.com/nsf/termbox-go v1.1.1
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/viper v1.13.0
 )
@@ -17,6 +18,7 @@ require (
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
